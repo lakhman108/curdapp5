94f0d4eddce8200035a90d3ac4c65ebed418be7c
kuldip rupsangbhai parmar <kuldiprparmar9759@gmail.com>
2024-02-14 20:01:44 +0530

    cunstomUser added

--- canteen/models.py
+++ canteen/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # address = models.CharField(max_length=255, blank=True, null=True)
+    mobile_number = models.CharField(max_length=10, blank=True, null=True)
+    
+class items(models.Model):
+    item_name = models.CharField(max_length=255, blank=False, null=False, )
